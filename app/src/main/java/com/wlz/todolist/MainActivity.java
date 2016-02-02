f036588139ce32f2dc02a84952b0f64f35d7438b
WaiLynnZaw <kolynn1500@gmail.com>
2016-02-02 10:19:56 +0630

    change the way to add task

--- app/src/main/java/com/wlz/todolist/MainActivity.java
+++ app/src/main/java/com/wlz/todolist/MainActivity.java
@@ -1,8 +1,12 @@
 package com.wlz.todolist;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -17,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "Main Activity";
     EditText todo_text;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Get ListView object from xml
         final ListView listView = (ListView) findViewById(R.id.listView);
-        todo_text = (EditText) findViewById(R.id.todo_text);
         // Create a new Adapter
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1);
@@ -58,11 +60,41 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                new Firebase("https://mtodolist.firebaseio.com/todoItems")
-                        .push()
-                        .child("text")
-                        .setValue(todo_text.getText().toString());
+                addTask();
+            }
+        });
 
+    }
+    private void addTask(){
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Enter task:")
+                .setView(R.layout.input_dialog)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new Firebase("https://mtodolist.firebaseio.com/todoItems")
+                                .push()
+                                .child("text")
+                                .setValue(todo_text.getText().toString());
+                    }
+                }).show();
+        todo_text = (EditText) alertDialog.findViewById(R.id.edit_mail);
+        todo_text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (todo_text.getText().toString().length() != 0) {
+
+                } else {
+                    todo_text.setError("Task should not be blank");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
     }
